f1413faedbd738ea7bc979a5cc5da9192e2f1595
Mary <43437572+mari4orl@users.noreply.github.com>
2020-02-24 19:00:16 +0300

    форма+попап

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,19 @@
 var link = document.querySelector('.search-link');
 var popup = document.querySelector('.popup');
 
+var form = popup.querySelector('form');
+var inputs = popup.querySelectorAll("[type=text]");
+
 link.addEventListener('click', function (evt) {
   evt.preventDefault();
   popup.classList.toggle('popup-close');
-  popup.classList.toggle('popup');
+});
+
+form.addEventListener("submit", function (evt) {
+  inputs.forEach(function(input) {
+    if (!input.value) {
+      evt.preventDefault();
+      console.log("Нужно ввести значения в поля");
+    }
+  });
 });
